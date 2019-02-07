650daaed0384af5515d11bd85f66efac6d8991aa
Helios Ciancio <eshiol@users.noreply.github.com>
2019-02-07 16:07:56 +0100

    fix

--- plugins/content/setimages/enable.php
+++ plugins/content/setimages/enable.php
@@ -1,16 +1,16 @@
 <?php
 /**
- * @package		Joomla.plugins
+ * @package		Joomla.Plugin
  * @subpackage	plg_content_setimages
  *
  * @author		Helios Ciancio <info@eshiol.it>
  * @link		http://www.eshiol.it
  * @copyright	Copyright (C) 2013 - 2019 Helios Ciancio. All Rights Reserved
  * @license		http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL v3
- * plg_content_setimages is free software.  This version may have been modified
- * pursuant  to  the GNU General Public License, and as distributed it includes 
- * or  is  derivative of works licensed under the GNU General Public License or
- * other free or open source software licenses.
+ * Content - Set Images is free software. This version may have been modified
+ * pursuant to the GNU General Public License, and as distributed it includes
+ * or  is  derivative  of works licensed under the GNU General Public License
+ * or other free or open source software licenses.
  */
 
 // no direct access
